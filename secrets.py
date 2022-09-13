bd52cc01fa9752377393c31df605ed566364b39b
c0rndaddy <73967955+c0rndaddy@users.noreply.github.com>
2022-09-13 17:56:17 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -8,6 +8,6 @@ secrets = {
     "broker": "test.mosquitto.org",
     "port": 1883,
     "timezone": "America/Toronto",
-    "aio_username": "jasmineh",
-    "aio_key": "aio_zdXE25aa2dRFsD4qYkHoqP9aORp3"
+    "aio_username": "user",
+    "aio_key": ""
 }
